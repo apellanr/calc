a758fb9977623aa2a29109b4d8fab7d6d6745579
apellanr <ryan.apellanes@gmail.com>
2017-04-26 17:09:54 -0700

    reference issue to constructor has been negated. working on to perform basic operations

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 $(document).ready(newCalculation);
 
-
-let calc = null;
+let calculation = null;
 
 function newCalculation() {
-    calc = new Calculator();
-    calc.init();
+    calculation = new Calculator();
+    calculation.init();
 }
 
 function Calculator() {
     var self = this;
-    this.inputArray = [''];
+    this.inputArray = [];
     this.firstOperand = null;
     this.lastOperand = null;
     this.decimalUsed = false;
@@ -18,7 +17,6 @@ function Calculator() {
     this.displayDiv = null;
     this.init = function() {
         this.number = $(".number");
-        console.log("init number pressed: ", this);
         this.operator = $(".operator");
         this.decimal = $(".decimal");
         this.equals = $(".equals");
@@ -28,22 +26,60 @@ function Calculator() {
         this.eventHandlers();
     };
 
+    // **** EVENT HANDLERS **** //
     this.eventHandlers = function() {
         // .bind changes context of this manually
         this.number.on('click', this.numberClicked);
+        this.operator.on('click', this.operatorClicked);
+        this.equals.click(this.solveCalculation);
+        this.backspace.click()
     };
 
-    // when number was being pressed was receiving an error of 'text' undefined
-    // this was referring to the constructor object rather than the button
-    this.numberClicked = function() {
-        console.log("number has been clicked");
-        this.currentNumber = this.number.text();
-        console.log(this.currentNumber);
+    // **** HANDLE NUMBER CLICK **** //
+    this.numberClicked = function(number) {
+        console.log("number was clicked ");
+        self.inputArray.push($(this).text()); // this was referring to the button clicked rather than the constructor object
+        self.displayInput(); // needed to use 'self' to reference the constructor again
     };
 
+    // **** HANDLE OPERATOR CLICK **** //
+    this.operatorClicked = function() {
+        console.log("operator was clicked");
+        self.inputArray.push($(this).text());
+        // this.displayInput();
+    };
+
+    // **** PERFORM LOGIC **** //
+    this.doMath = function(num1, num2, operator) {
+    var num1 = parseFloat(num1);
+    var num2 = parseFloat(num2);
+
+    switch(operator) {
+        case '+':
+            var result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        default:
+            console.log("invalid entry");
+            break;
+    }
+    return result;
+};
+
+    // **** HANDLE OUTPUT **** //
     this.displayInput = function() {
-        // this.displayDiv.text();
+        // console.log('displaying output');
+        var calcDisplay = self.inputArray.join('');
+        this.displayDiv.text(calcDisplay);
     };
 
+    // **** HANDLE EQUAL SIGN CLICK **** //
+    this.solveCalculation = function() {
+
+    };
+
+    // **** ADVANCED OPERATIONS **** //
 
 }
